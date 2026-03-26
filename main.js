b1ce2462af3d9f37f2e0e79da9c94a849b6816b1
swollitzer <s.g.wollitzer@wustl.edu>
2026-03-26 01:24:17 -0500

    done?

--- main.js
+++ main.js
@@ -20,3 +20,21 @@ particlesJS("particles-js", {
     }
   }
 });
+const faders = document.querySelectorAll('.fade-in-section');
+
+  const appearOptions = {
+    threshold: 0.1, 
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
